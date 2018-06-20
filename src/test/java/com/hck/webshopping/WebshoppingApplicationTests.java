6e104a0b6b12a23e832b9e6564090bf213324963
HAPPYDEER <wlcsqyn@qq.com>
2018-06-20 20:37:40 +0800

    初始化项目

---
+++ src/test/java/com/hck/webshopping/WebshoppingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hck.webshopping;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebshoppingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
